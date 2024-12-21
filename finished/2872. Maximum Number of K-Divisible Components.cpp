da0093898e2a918d941acb10b70953748a0b78ba
Seann <seanyu0425@gmail.com>
2024-12-21 08:44:06 +0800

    2872

---
+++ finished/2872. Maximum Number of K-Divisible Components.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+    vector<vector<int>> adj;
+    int ans = 0;
+    int dfs(int node, int parent, int k, vector<int> &values) {
+        auto value = values[node];
+        for (const auto &child : adj[node]) {
+            if (child == parent) continue;
+            value += dfs(child, node, k, values);
+        }
+        if (value % k) return value % k;
+        ans++;
+        return 0;
+    }
+public:
+    int maxKDivisibleComponents(size_t n, vector<vector<int>> &edges, vector<int> &values, int k) {
+        adj.resize(n);
+        for (const auto &edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        dfs(0, -1, k, values);
+        return ans;
+    }
+};
