654d50a19c4119aec7140664553e068a8a59f6b4
Seann <seanyu0425@gmail.com>
2025-01-18 21:34:20 +0800

    646

---
+++ finished/646. Maximum Length of Pair Chain.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+#include <ranges>
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>> &pairs) {
+        sort(pairs.begin(), pairs.end(), [](const auto &lhs, const auto &rhs) {
+            if (lhs[1] == rhs[1]) return lhs[0] < rhs[0];
+            return lhs[1] < rhs[1];
+        });
+        auto cur = pairs[0][1], ans = 1;
+        for (const auto &pair : pairs | views::drop(1))
+            if (pair[0] > cur) cur = pair[1], ans++;
+        return ans;
+    }
+};
