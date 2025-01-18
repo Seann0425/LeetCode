7cd3183b862f0e2a9e371b6f2c8c071c9409483e
Seann <seanyu0425@gmail.com>
2025-01-18 08:34:36 +0800

    397

---
+++ 324. Wiggle Sort II.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    void wiggleSort(vector<int> &nums) {
+        array<int, 5005> cnt{};
+        for (const auto &i : nums) cnt[i]++;
+        const auto n = nums.size();
+        auto i = 0uz, j = 0uz;
+        for (; i < n; i += 2) {
+            while (!cnt[j]) j++;
+            nums[i] = j;
+            cnt[j]--;
+        }
+        for (i = 1; i < n; i += 2) {
+            while (!cnt[j]) j++;
+            nums[i] = j;
+            cnt[j]--;
+        }
+    }
+};
