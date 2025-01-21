cb1f6cb3c8e391a88afed9f57ff0c8297dc54091
Seann <seanyu0425@gmail.com>
2025-01-21 18:35:10 +0800

    910

---
+++ finished/910. Smallest Range II.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    int smallestRangeII(vector<int> &nums, int k) {
+        const auto n = nums.size();
+        sort(nums.begin(), nums.end());
+        auto ans = nums[n - 1] - nums[0];
+        for (auto i = 0uz; i < n - 1; i++) {
+            auto a = nums[i], b = nums[i + 1];
+            auto high = max(nums[n - 1] - k, a + k);
+            auto low = min(nums[0] + k, b - k);
+            ans = min(ans, high - low);
+        }
+        return ans;
+    }
+};
