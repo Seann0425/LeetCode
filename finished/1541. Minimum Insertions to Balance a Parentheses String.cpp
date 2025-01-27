6cd7240dc44239e615b16b4b7044b444cba78a15
Seann <seanyu0425@gmail.com>
2025-01-27 22:12:04 +0800

    1541

---
+++ finished/1541. Minimum Insertions to Balance a Parentheses String.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    int minInsertions(string s) {
+        auto opening = 0, closing = 0, ans = 0;
+        for (const auto &c : s) {
+            if (c == '(') {
+                if (closing) ans++, closing--;
+                else opening++;
+            } else {
+                if (!opening) ans++, opening++, closing++;
+                else if (closing) closing--, opening--;
+                else closing++;
+            }
+        }
+        return ans + opening * 2 - closing;
+    }
+};
