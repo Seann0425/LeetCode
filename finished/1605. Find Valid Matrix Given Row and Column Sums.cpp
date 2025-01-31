1f1159d7215d24642af39c9e129997cbb82561be
Seann <seanyu0425@gmail.com>
2025-01-31 08:54:08 +0800

    1605

---
+++ finished/1605. Find Valid Matrix Given Row and Column Sums.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {
+    }
+    ListNode(int x) : val(x), next(nullptr) {
+    }
+    ListNode(int x, ListNode *next) : val(x), next(next) {
+    }
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
+    }
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& row_sum, vector<int>& col_sum) {
+        const auto m = row_sum.size(), n = col_sum.size();
+        vector<vector<int>> mat(m, vector<int>(n, 0));
+        for (auto i = 0uz; i < m; i++) {
+            for (auto j = 0uz; j < n; j++) {
+                mat[i][j] = min(row_sum[i], col_sum[j]);
+                row_sum[i] -= mat[i][j];
+                col_sum[j] -= mat[i][j];
+            }
+        }
+        return mat;
+    }
+};
