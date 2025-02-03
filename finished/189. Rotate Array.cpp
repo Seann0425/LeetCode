bc6618776de4074267ec508f943e25a4dac5b0dd
Seann <seanyu0425@gmail.com>
2025-02-03 11:18:54 +0800

    189

---
+++ finished/189. Rotate Array.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+    void reverse(vector<int> &nums, size_t start, size_t end) {
+        while (start < end) swap(nums[start++], nums[end--]);
+    }
+public:
+    void rotate(vector<int> &nums, size_t k) {
+        const auto n = nums.size();
+        if (n == 1) return;
+        k %= n;
+        if (!k) return;
+        reverse(nums, 0, n - 1);
+        if (k > 1) reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+};
