1846d04479571f258c19196bd85ab2bafbcda742
Seann <seanyu0425@gmail.com>
2025-02-05 11:55:33 +0800

    1833

---
+++ finished/1833. Maximum Ice Cream Bars.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+    static constexpr size_t max_n = 100005;
+public:
+    int maxIceCream(vector<int> &costs, int coins) {
+        array<int, max_n> cnt{};
+        for (const auto &i : costs) cnt[i]++;
+        auto ans = 0;
+        for (size_t i = 0uz; i < max_n; i++) {
+            if (!cnt[i]) continue;
+            const auto num = min(cnt[i], coins / static_cast<int>(i));
+            ans += num;
+            coins -= num * i;
+            if (coins < i) break;
+        }
+        return ans;
+    }
+};
