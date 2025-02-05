31ed6ff313c6aa21d7e97ed04d71fb3024557ff8
Seann <seanyu0425@gmail.com>
2025-02-05 10:31:15 +0800

    1790

--- finished/1790. Check if One String Swap Can Make Strings Equal.cpp
+++ finished/1790. Check if One String Swap Can Make Strings Equal.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -45,21 +39,19 @@ public:
 /*the solution should start from below*/
 
 class Solution {
-    bool isClear(vector<int> &v) {
-        for (int i = 0; i < 26; i++)
-            if (v[i] != 0) return false;
-        return true;
-    }
 public:
     bool areAlmostEqual(string s1, string s2) {
-        vector<int> counting(26, 0);
-        int difference = 0;
-        for (int i = 0; i < s1.size(); i++) {
-            counting[s1[i] - 'a']++;
-            counting[s2[i] - 'a']--;
-            if (s1[i] != s2[i]) difference++;
-            if (difference > 2) return false;
-        }
-        return difference == 0 || (difference == 2 && isClear(counting));
+        array<int, 26> freq{};
+        auto diff = 0;
+        const auto n = s1.size();
+        for (auto i = 0uz; i < n; i++)
+            if (s1[i] != s2[i]) {
+                diff++;
+                freq[s1[i] - 'a']++;
+                freq[s2[i] - 'a']--;
+                if (diff > 2) return false;
+            }
+        return !diff or
+               (diff == 2 and all_of(freq.begin(), freq.end(), [](const auto &f) { return !f; }));
     }
 };
