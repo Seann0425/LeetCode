a7c73da0b58bcbe4a363329d5d54c5da7c19b520
Seann <seanyu0425@gmail.com>
2025-02-16 22:29:13 +0800

    429

---
+++ finished/429. N-ary Tree Level Order Traversal.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val) { val = _val; }
+
+    Node(int _val, vector<Node *> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node *root) {
+        if (!root) return {};
+        vector<vector<int>> ans;
+        queue<Node *> q;
+        q.push(root);
+        while (!q.empty()) {
+            auto size = q.size();
+            ans.emplace_back();
+            while (size--) {
+                auto node = q.front();
+                q.pop();
+                ans.back().push_back(node->val);
+                for (const auto &child : node->children) q.push(child);
+            }
+        }
+        return ans;
+    }
+};
