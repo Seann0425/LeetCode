04705035f720c26e19cfee7f165d9f59a6d0e830
Seann <seanyu0425@gmail.com>
2025-02-18 14:11:40 +0800

    3066 update

--- finished/3066. Minimum Operations to Exceed Threshold Value II.cpp
+++ finished/3066. Minimum Operations to Exceed Threshold Value II.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -44,23 +38,26 @@ public:
 
 /*the solution should start from below*/
 
+#include <ranges>
 class Solution {
 public:
     int minOperations(vector<int> &nums, int k) {
+        auto threshold = [&](int n) { return n < k; };
         priority_queue<int, vector<int>, greater<int>> pq;
-        for (const auto &i : nums)
-            pq.push(i);
-        int cnt = 0;
-        while (pq.size() >= 2 && pq.top() < k) {
-            int a = pq.top();
+        for (const auto &i : nums | views::filter(threshold)) pq.push(i);
+        auto ans = 0;
+        while (!pq.empty() and ++ans) {
+            if (pq.size() == 1) break;
+            auto a = pq.top() * 2;
             pq.pop();
-            int b = pq.top();
+            if (!threshold(a)) {
+                pq.pop();
+                continue;
+            }
+            auto b = a + pq.top();
             pq.pop();
-            cnt++;
-            if (b >= k) break;
-            if (static_cast<long long>(a) * 2 + b > k) pq.emplace(INT_MAX);
-            else pq.emplace(a * 2 + b);
+            if (threshold(b)) pq.push(b);
         }
-        return cnt;
+        return ans;
     }
 };
