63d35fc9ade49b5186cf9a3c97637fe77e1a0f16
Seann <seanyu0425@gmail.com>
2025-02-26 09:31:45 +0800

    1749

---
+++ finished/1749. Maximum Absolute Sum of Any Subarray.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+#pragma GCC optimize("O3", "unroll-loops")
+#include <ranges>
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+// kadane's algorithm
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int> &nums) {
+        auto max_sum = nums[0], min_sum = nums[0];
+        auto sum_mx = nums[0], sum_mn = nums[0];
+        for (const auto &i : nums | views::drop(1)) {
+            sum_mx = max(i, sum_mx + i);
+            max_sum = max(max_sum, sum_mx);
+            sum_mn = min(i, sum_mn + i);
+            min_sum = min(min_sum, sum_mn);
+        }
+        return max(abs(max_sum), abs(min_sum));
+    }
+};
