cd4b521e7fecedcf4d08c77efe346f566467ec91
Seann <seanyu0425@gmail.com>
2025-02-28 19:37:35 +0800

    3016

---
+++ finished/3016. Minimum Number of Pushes to Type Word II.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+#pragma GCC optimize("O3", "unroll-loops")
+#include <ranges>
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        array<int, 26> freq{};
+        for (const auto &c : word) ++freq[c - 'a'];
+        sort(freq.begin(), freq.end(), greater<int>());
+        auto ans = 0;
+        for (auto i = 0uz; i < 26uz; ++i) {
+            if (!freq[i]) break;
+            ans += freq[i] * (i / 8 + 1);
+        }
+        return ans;
+    }
+};
