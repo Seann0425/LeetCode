1a82fc9b1edb2363f12df6f985c7b6694979a571
Seann <seanyu0425@gmail.com>
2025-03-01 14:26:02 +0800

    2460 update

--- finished/2460. Apply Operations to an Array.cpp
+++ finished/2460. Apply Operations to an Array.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -47,15 +41,19 @@ public:
 class Solution {
 public:
     vector<int> applyOperations(vector<int> &nums) {
-        // 這邊在處理陣列
-        for (int i = 0; i < nums.size() - 1; i++) {
+        const auto n = nums.size();
+        for (auto i = 0uz; i < n - 1; ++i)
             if (nums[i] == nums[i + 1]) {
                 nums[i] *= 2;
                 nums[i + 1] = 0;
             }
+        for (auto i = 0uz, j = 0uz; i < n; ++i) {
+            if (j == n) nums[i] = 0;
+            else {
+                while (j < n and !nums[j]) ++j;
+                nums[i] = j == n ? 0 : nums[j++];
+            }
         }
-        // 把0移到後面
-        stable_sort(nums.begin(), nums.end(), [](const int &a, const int &b) { return (a != 0 && b == 0); });
         return nums;
     }
 };
