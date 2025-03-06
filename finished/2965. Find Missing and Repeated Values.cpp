d40cac0e9b843eecfc5be230110ac36258c9ebc9
Seann <seanyu0425@gmail.com>
2025-03-06 09:49:02 +0800

    2965 update

--- finished/2965. Find Missing and Repeated Values.cpp
+++ finished/2965. Find Missing and Repeated Values.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -47,20 +41,18 @@ public:
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>> &grid) {
-        const int n = grid.size();
-        vector<bool> occurOnce(n * n);
-        vector<int> ans(2, 0);
-        int gridSum = 0;
-        for (const auto &v : grid) {
-            for (const auto &i : v) {
-                gridSum += i;
-                occurOnce[i - 1] = !occurOnce[i - 1];
-                if (occurOnce[i - 1] == false) {
-                    ans[0] = i;
-                }
+        const auto n = static_cast<long long>(grid.size() * grid.size());
+        auto sum = 0ll, sqr_sum = 0ll;
+        for (const auto &row : grid) {
+            for (const auto &x : row) {
+                sum += x;
+                sqr_sum += static_cast<long long>(x) * x;
             }
         }
-        ans[1] = (n * n) * (n * n + 1) / 2 - gridSum + ans[0];
-        return ans;
+        auto sum_diff = sum - n * (n + 1) / 2;
+        auto sqr_sum_diff = sqr_sum - n * (n + 1) * (2 * n + 1) / 6;
+        auto repeated = static_cast<int>((sqr_sum_diff / sum_diff + sum_diff) / 2);
+        auto missing = static_cast<int>((sqr_sum_diff / sum_diff - sum_diff) / 2);
+        return {repeated, missing};
     }
 };
