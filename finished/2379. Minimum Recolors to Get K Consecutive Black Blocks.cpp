78d2ce629e8d91fef3db21f38bf48ea76173f290
Seann <seanyu0425@gmail.com>
2025-03-09 10:25:02 +0800

    2379 update

--- finished/2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
+++ finished/2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -46,21 +40,16 @@ public:
 
 class Solution {
 public:
-    int minimumRecolors(string blocks, int k) {
-        // sliding window
-        vector<int> counter(2, 0); // 0 for white, 1 for black
-        for (int i = 0; i < k; i++) {
-            if (blocks[i] == 'W') counter[0]++;
-            else counter[1]++;
+    int minimumRecolors(string blocks, size_t k) {
+        pair<int, int> cnt{};
+        const auto n = blocks.size();
+        for (auto i = 0uz; i < k; ++i) ++(blocks[i] == 'W' ? cnt.first : cnt.second);
+        auto ans = cnt.first;
+        for (auto i = k; i < n; ++i) {
+            ++(blocks[i] == 'W' ? cnt.first : cnt.second);
+            --(blocks[i - k] == 'W' ? cnt.first : cnt.second);
+            ans = min(ans, cnt.first);
         }
-        int minimum = counter[0];
-        for (int i = 1; i <= blocks.size() - k; i++) {
-            if (blocks[i - 1] == 'W') counter[0]--;
-            else counter[1]--;
-            if (blocks[i + k - 1] == 'W') counter[0]++;
-            else counter[1]++;
-            minimum = min(minimum, counter[0]);
-        }
-        return minimum;
+        return ans;
     }
 };
