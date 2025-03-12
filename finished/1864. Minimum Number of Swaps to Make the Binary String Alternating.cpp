94bc98a71e891b18d8879418ca3a953030d0601d
Seann <seanyu0425@gmail.com>
2025-03-12 09:31:11 +0800

    1864

---
+++ finished/1864. Minimum Number of Swaps to Make the Binary String Alternating.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+#pragma GCC optimize("O3", "unroll-loops")
+#include <ranges>
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+public:
+    int minSwaps(string s) {
+        const auto n = s.size();
+        auto zero_cnt = count(s.begin(), s.end(), '0'), one_cnt = static_cast<int>(n) - zero_cnt;
+        auto one_missed = 0, zero_missed = 0;
+        if (abs(zero_cnt - one_cnt) > 1) return -1;
+        for (auto i = 0uz; i < n; i += 2) {
+            if (s[i] != '0') ++zero_missed;
+            if (s[i] != '1') ++one_missed;
+        }
+        return zero_cnt == one_cnt  ? min(zero_missed, one_missed)
+               : zero_cnt > one_cnt ? zero_missed
+                                    : one_missed;
+    }
+};
