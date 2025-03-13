78f6042fc0ff0842f841467761a5e300999dd49c
Seann <seanyu0425@gmail.com>
2025-03-13 10:07:46 +0800

    2522

---
+++ finished/2522. Partition String Into Substrings With Values at Most K.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+#pragma GCC optimize("O3", "unroll-loops")
+#include <ranges>
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+public:
+    int minimumPartition(string s, int k) {
+        auto ans = 0;
+        auto num = 0ll;
+        for (const auto &c : s) {
+            const auto x = c - '0';
+            if (num * 10 + x > k) {
+                if (x > k) return -1;
+                ++ans;
+                num = x;
+            } else num = 10 * num + x;
+        }
+        return ans + 1;
+    }
+};
