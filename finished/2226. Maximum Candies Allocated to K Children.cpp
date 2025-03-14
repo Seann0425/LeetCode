9947406baaa09bd0fc605a788f70eccd1446c063
Seann <seanyu0425@gmail.com>
2025-03-14 09:34:49 +0800

    2226

---
+++ finished/2226. Maximum Candies Allocated to K Children.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+#pragma GCC optimize("O3", "unroll-loops")
+#include <ranges>
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+    bool get_candy(const vector<int> &candies, long long m, long long k) {
+        // return true if k kids can have m candies
+        for (const auto &candy : candies) {
+            k -= candy / m;
+            if (k <= 0) return true;
+        }
+        return false;
+    }
+public:
+    int maximumCandies(vector<int> &candies, long long k) {
+        const auto n = candies.size();
+        auto l = 0ll, r = 10'000'000ll;
+        while (l < r) {
+            auto m = l + (r - l) / 2 + 1;
+            if (get_candy(candies, m, k)) l = m;
+            else r = m - 1;
+        }
+        return static_cast<int>(l);
+    }
+};
