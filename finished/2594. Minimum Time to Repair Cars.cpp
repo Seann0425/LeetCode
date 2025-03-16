efe0fb081172469854523ab0c833dc41621e1603
Seann <seanyu0425@gmail.com>
2025-03-16 12:43:56 +0800

    2594

---
+++ finished/2594. Minimum Time to Repair Cars.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+#pragma GCC optimize("O3", "unroll-loops")
+#include <ranges>
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+    size_t n;
+    // can repair all cars in time
+    bool can_repair(const vector<int> &ranks, int cars, long long time) {
+        auto cnt = 0ll;
+        for (const auto &rank : ranks) {
+            cnt += static_cast<long long>(pow(time / rank, 0.5));
+            if (cnt >= cars) return true;
+        }
+        return false;
+    }
+public:
+    long long repairCars(vector<int> &ranks, int cars) {
+        n = ranks.size();
+        auto l = 1ll, r = numeric_limits<long long>::max();
+        while (l < r) {
+            auto m = l + (r - l) / 2;
+            if (can_repair(ranks, cars, m)) r = m;
+            else l = m + 1;
+        }
+        return l;
+    }
+};
