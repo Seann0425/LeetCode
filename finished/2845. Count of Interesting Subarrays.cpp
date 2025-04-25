6e0b08c7b3f0e2e586f14ad9cf69e37532cc5cbc
Seann <seanyu0425@gmail.com>
2025-04-25 10:54:50 +0800

    2845

---
+++ finished/2845. Count of Interesting Subarrays.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+#pragma GCC optimize("O3", "unroll-loops")
+#include <ranges>
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int> &nums, int modulo, int k) {
+        const auto n = nums.size();
+        vector<long long> cnt(n + 1, 0);
+        for (auto i = 0uz; i < n; ++i) cnt[i + 1] = (cnt[i] + (nums[i] % modulo == k)) % modulo;
+        unordered_map<int, long long> prefix;
+        prefix[0] = 1;
+        auto ans = 0ll;
+        for (auto i = 0uz; i < n; ++i) {
+            ans += prefix[(cnt[i + 1] - k + modulo) % modulo];
+            ++prefix[cnt[i + 1]];
+        }
+        return ans;
+    }
+};
