ba4de12c51e3bc6eef06cb6bb5f1b45c9c7b2020
Seann <seanyu0425@gmail.com>
2025-05-14 10:47:18 +0800

    2115

---
+++ finished/2115. Find All Possible Recipes from Given Supplies.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+#pragma GCC optimize("O3", "unroll-loops")
+#include <ranges>
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string> &recipes, vector<vector<string>> &ingredients,
+                                  vector<string> &supplies) {
+        unordered_set<string> supply_set(supplies.begin(), supplies.end());
+        queue<size_t> recipe_queue;
+        for (auto i = 0uz; i < recipes.size(); ++i) recipe_queue.push(i);
+        vector<string> ans;
+        auto last_size = numeric_limits<size_t>::max();
+        while (supply_set.size() > last_size or last_size == numeric_limits<size_t>::max()) {
+            last_size = supply_set.size();
+            auto size = recipe_queue.size();
+            while (size--) {
+                auto i = recipe_queue.front();
+                recipe_queue.pop();
+                auto create = true;
+                for (const auto &ingredient : ingredients[i]) {
+                    if (supply_set.contains(ingredient)) continue;
+                    create = false;
+                    break;
+                }
+                if (create) {
+                    ans.push_back(recipes[i]);
+                    supply_set.insert(recipes[i]);
+                } else recipe_queue.push(i);
+            }
+        }
+        return ans;
+    }
+};
