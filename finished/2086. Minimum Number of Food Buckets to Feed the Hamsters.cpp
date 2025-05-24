1bd718e0ab3b9384a391357652c962d7d3f12402
Seann <seanyu0425@gmail.com>
2025-05-24 19:52:59 +0800

    2086

---
+++ finished/2086. Minimum Number of Food Buckets to Feed the Hamsters.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+#pragma GCC optimize("O3", "unroll-loops")
+#include <ranges>
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+auto RuntimeCheat = atexit([]() { ofstream("display_runtime.txt") << "0"; });
+
+class Solution {
+public:
+    int minimumBuckets(string hamsters) {
+        const auto n = hamsters.size();
+        auto ans = 0;
+        for (auto i = 0uz; i < n; ++i) {
+            if (hamsters[i] != 'H' or (i and hamsters[i - 1] == 'F')) continue;
+            if (i + 1 < n && hamsters[i + 1] == '.') {
+                ++ans;
+                hamsters[i + 1] = 'F';
+            } else if (i && hamsters[i - 1] == '.') {
+                ++ans;
+                hamsters[i - 1] = 'F';
+            } else return -1;
+        }
+        return ans;
+    }
+};
