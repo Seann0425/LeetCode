8f07a9ca1ef33e32613c1931b065acfb4f5024a0
Seann <seanyu0425@gmail.com>
2025-05-27 10:54:18 +0800

    2894

--- finished/2894. Divisible and Non-divisible Sums Difference.cpp
+++ finished/2894. Divisible and Non-divisible Sums Difference.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -47,10 +41,6 @@ public:
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int num1 = 0, num2;
-        for (int i = 1; i <= n; i++)
-            if (i % m != 0) num1 += i;
-        num2 = (n * (1 + n)) / 2 - num1;
-        return num1 - num2;
+        return n * (n + 1) / 2 - m * (n / m) * ((n / m) + 1) / 2 - m * (n / m) * ((n / m) + 1) / 2;
     }
 };
