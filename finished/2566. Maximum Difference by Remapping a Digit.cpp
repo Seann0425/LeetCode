d9f2b18fbc2fa672fab8d54fb8b0a750e0a1c2f7
Seann <seanyu0425@gmail.com>
2025-06-14 22:58:20 +0800

    2566 update

--- finished/2566. Maximum Difference by Remapping a Digit.cpp
+++ finished/2566. Maximum Difference by Remapping a Digit.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -47,27 +41,15 @@ public:
 class Solution {
 public:
     int minMaxDifference(int num) {
-        string mx = to_string(num);
-        char yo = '@';
-        for (auto &digit : mx) {
-            if (digit != '9' && yo == '@') {
-                yo = digit;
-                digit = '9';
-            } else if (digit == yo) {
-                digit = '9';
-            }
+        string maximum = to_string(num);
+        string minimum = maximum;
+        auto pos = maximum.find_first_not_of('9');
+        if (pos != string::npos) {
+            auto a = maximum[pos];
+            replace(maximum.begin(), maximum.end(), a, '9');
         }
-        string mn = to_string(num);
-        yo = '@';
-        for (auto &digit : mn) {
-            if (digit != '0' && yo == '@') {
-                yo = digit;
-                digit = '0';
-            } else if (digit == yo) {
-                digit = '0';
-            }
-        }
-
-        return stoi(mx) - stoi(mn);
+        auto b = minimum[0];
+        replace(minimum.begin(), minimum.end(), b, '0');
+        return stoi(maximum) - stoi(minimum);
     }
 };
