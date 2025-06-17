fa642575381c9e5cb3c53253da4aff33d67897cd
Seann <seanyu0425@gmail.com>
2025-06-17 14:13:26 +0800

    3403

---
+++ finished/3403. Find the Lexicographically Largest String From the Box I.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+#pragma GCC optimize("O3", "unroll-loops")
+#include <ranges>
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+auto RuntimeCheat = atexit([]() { ofstream("display_runtime.txt") << "0"; });
+
+class Solution {
+public:
+    string answerString(string word, size_t numFriends) {
+        if (numFriends == 0) return word;
+        const auto n = word.size();
+        string ans;
+        ans.reserve(n - numFriends + 1);
+        for (auto i = 0uz; i < n; ++i)
+            ans = max(ans, word.substr(i, min(n - i, n - numFriends + 1)));
+        return ans;
+    }
+};
