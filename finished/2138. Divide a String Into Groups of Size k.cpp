cf01fdcefe9eeb3b1af32219fb108b475daac7a8
Seann <seanyu0425@gmail.com>
2025-06-22 08:25:50 +0800

    2138 update

--- finished/2138. Divide a String Into Groups of Size k.cpp
+++ finished/2138. Divide a String Into Groups of Size k.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -46,15 +40,13 @@ public:
 
 class Solution {
 public:
-    vector<string> divideString(string s, int k, char fill) {
-        int n = s.size();
+    vector<string> divideString(string s, size_t k, char fill) {
+        const auto n = s.size();
         vector<string> ans;
-        for (int i = 0; i < n / k; i++)
-            ans.push_back(s.substr(i * k, k));
+        for (auto i = 0uz; i < n / k; ++i) ans.push_back(s.substr(i * k, k));
         if (n % k) {
             string last = s.substr(n / k * k, n % k);
-            while (last.size() < k)
-                last.push_back(fill);
+            while (last.size() < k) last.push_back(fill);
             ans.push_back(last);
         }
         return ans;
