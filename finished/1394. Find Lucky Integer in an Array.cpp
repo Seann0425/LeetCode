f37dae5b23c7ee54fba5cac7d2f7424e3d077118
Seann0425 <seanyu0425@gmail.com>
2025-07-05 14:21:47 +0800

    1394

--- finished/1394. Find Lucky Integer in an Array.cpp
+++ finished/1394. Find Lucky Integer in an Array.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -43,3 +37,15 @@ public:
 };
 
 /*the solution should start from below*/
+
+class Solution {
+public:
+    int findLucky(vector<int> &arr) {
+        array<int, 505> freq{};
+        for (const auto &i : arr) ++freq[i];
+        auto ans = -1;
+        for (auto i = 1uz; i < 505uz; ++i)
+            if (i == freq[i]) ans = freq[i];
+        return ans;
+    }
+};
