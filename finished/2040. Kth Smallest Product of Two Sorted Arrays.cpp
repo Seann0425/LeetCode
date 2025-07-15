2516d54816995e36d5226359f2aaa0abe46cd5a7
Seann0425 <seanyu0425@gmail.com>
2025-07-15 15:12:09 +0800

    2040

--- finished/2040. Kth Smallest Product of Two Sorted Arrays.cpp
+++ finished/2040. Kth Smallest Product of Two Sorted Arrays.cpp
@@ -66,14 +66,52 @@ auto RuntimeCheat = atexit([]() { ofstream("display_runtime.txt") << "0"; });
 class Solution {
 public:
     long long kthSmallestProduct(vector<int> &nums1, vector<int> &nums2, long long k) {
-        const auto m = nums1.size(), n = nums2.size();
-        auto zero1 = static_cast<size_t>(ranges::lower_bound(nums1, 0) - nums1.begin());
-        auto zero2 = static_cast<size_t>(ranges::lower_bound(nums2, 0) - nums2.begin());
-
+        int n1 = nums1.size(), n2 = nums2.size();
+        int pos1 = 0, pos2 = 0;
+        pos1 = static_cast<int>(ranges::lower_bound(nums1, 0) - nums1.begin());
+        pos2 = static_cast<int>(ranges::lower_bound(nums2, 0) - nums2.begin());
         long long left = -1e10, right = 1e10;
         while (left <= right) {
-            auto mid = left + (right - left) / 2;
-            auto count = 0ll;
-            for () }
+            long long mid = (left + right) / 2;
+            long long count = 0;
+            for (int i1 = 0, i2 = pos2 - 1; i1 < pos1 && i2 >= 0;) {
+                if (static_cast<long long>(nums1[i1]) * nums2[i2] > mid) {
+                    i1++;
+                } else {
+                    count += pos1 - i1;
+                    i2--;
+                }
+            }
+            for (int i1 = pos1, i2 = n2 - 1; i1 < n1 && i2 >= pos2;) {
+                if (static_cast<long long>(nums1[i1]) * nums2[i2] > mid) {
+                    i2--;
+                } else {
+                    count += i2 - pos2 + 1;
+                    i1++;
+                }
+            }
+            for (int i1 = 0, i2 = pos2; i1 < pos1 && i2 < n2;) {
+                if (static_cast<long long>(nums1[i1]) * nums2[i2] > mid) {
+                    i2++;
+                } else {
+                    count += n2 - i2;
+                    i1++;
+                }
+            }
+            for (int i1 = pos1, i2 = 0; i1 < n1 && i2 < pos2;) {
+                if (static_cast<long long>(nums1[i1]) * nums2[i2] > mid) {
+                    i1++;
+                } else {
+                    count += n1 - i1;
+                    i2++;
+                }
+            }
+            if (count < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 };
