b763331417d91cbb3a25a6ced79c2730b68a9ac4
Seann0425 <seanyu0425@gmail.com>
2025-07-27 04:48:38 +0800

    1041

---
+++ finished/1041. Robot Bounded In Circle.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+#include <ranges>
+#ifndef DEBUG
+#define DEBUG
+template <typename T>
+void debug(const vector<T> &v) {
+    if (v.empty()) {
+        cout << "[]"s << endl;
+        return;
+    }
+    cout << '[' << v.front();
+    for (const auto &x : v | views::drop(1)) cout << ',' << x;
+    cout << "]" << endl;
+}
+void debug(const string &s) { cout << '"' << s << '"' << endl; }
+#endif
+#pragma GCC optimize("O3", "unroll-loops")
+
+static const auto InitialOptimization = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+auto RuntimeCheat = atexit([]() { ofstream("display_runtime.txt") << "0"; });
+
+class Solution {
+    static constexpr array<int, 5> direction{0, 1, 0, -1, 0};
+public:
+    bool isRobotBounded(string instructions) {
+        auto x = 0, y = 0;
+        auto at = 0uz;
+        for (const auto &i : instructions) {
+            if (i == 'G') x += direction[at], y += direction[at + 1];
+            else if (i == 'L') at = at ? at - 1 : 3uz;
+            else at = (at + 1) % 4;
+        }
+        return !x and !y or at;
+    }
+};
