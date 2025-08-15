554144d557af10fe635ba04f26e62de70972874f
Seann0425 <seanyu0425@gmail.com>
2025-08-15 09:38:42 +0800

    2787

--- finished/342. Power of Four.cpp
+++ finished/342. Power of Four.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -47,7 +41,8 @@ public:
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (!n) return false;
-        return ((n & 0x55555555) == n && !(n & (n - 1)));
+        if (n <= 0) return false;
+        auto x = static_cast<unsigned>(n);
+        return has_single_bit(x) and !(countr_zero(x) % 2);
     }
 };
